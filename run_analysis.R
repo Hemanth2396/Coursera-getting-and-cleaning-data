d240935f9baf957d2a53694662a5f257a09b412e
Divya Shree H P <39622723+divya092@users.noreply.github.com>
2019-12-24 20:36:41 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -46,9 +46,9 @@ data_filtered <- dataset[, col_names_filtered]
 #Add descriptive values for activity labels
 data_filtered$activitylabel <- factor(data_filtered$activity, labels= c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", "SITTING", "STANDING", "LAYING"))
 
-features.colnames = grep("std\\(\\)|mean\\(\\)", column.names, value=TRUE)
+features_colnames = grep("std\\(\\)|mean\\(\\)", col_names, value=TRUE)
 
-data_melted <-melt(data_filtered, id = c('activitylabel', 'subject'), measure.vars = features.colnames)
+data_melted <-melt(data_filtered, id = c('activitylabel', 'subject'), measure.vars = features_colnames)
 data_mean <- dcast(data_melted, subject + activitylabel ~ variable, mean)
 
 #Creating a tidy dataset file
